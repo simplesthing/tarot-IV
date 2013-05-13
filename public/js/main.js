8fc32815d67b1e0bd3d851ac6d835c19b0921d1e
Ava <avacollins@comcast.net>
2013-05-13 12:03:17 -0700

    updates to reading style

--- public/js/main.js
+++ public/js/main.js
@@ -10,62 +10,62 @@ tarot.controller("tarotController", function($scope, $http){
 	
   $scope.updateSpread = function(dfd, id ){
     $('#'+ id +' img').attr('src', $scope.model.cards[$scope.model.card].image);
-    $('#reading').prepend($('<dt/>').html(id));
+    $('#reading').find('dt.active').removeClass('active');
+    $('#reading').prepend($('<dt/>').html(id).addClass('active'));
     dfd.resolve();
   }
   $scope.draw = function(){
     $scope.model.card =  Math.floor((Math.random() * 77)+1);
-
+    $('#reading').find('dd.active').removeClass('active');
     if($scope.model.card_count < 10 ){
      switch($scope.model.card_count){
       case 0:
       var dfd = $.Deferred();
-      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].situation));
+      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].situation).addClass('active'));
       dfd.done($scope.updateSpread(dfd, 'situation'));
       break;
       case 1:
       var dfd = $.Deferred();
-      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].challenge));
+      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].challenge).addClass('active'));
       dfd.done($scope.updateSpread(dfd, 'challenge'));
       break;
       case 2:
       var dfd = $.Deferred();
-      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].power));
+      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].power).addClass('active'));
       dfd.done($scope.updateSpread(dfd, 'power'));
       break;
       case 3:
       var dfd = $.Deferred();
-      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].foundation));
+      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].foundation).addClass('active'));
       dfd.done($scope.updateSpread(dfd, 'foundation'));
       break;
       case 4:
       var dfd = $.Deferred();
-      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].past));
+      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].past).addClass('active'));
       dfd.done($scope.updateSpread(dfd, 'past'));
       break;
       case 5:
       var dfd = $.Deferred();
-      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].future));
+      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].future).addClass('active'));
       dfd.done($scope.updateSpread(dfd, 'future'));
       break;
       case 6:
       var dfd = $.Deferred();
-      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].self));
-      dfd.done($scope.updateSpread(dfd, 'self'));
+      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].self).addClass('active'));
       break;
       case 7:
       var dfd = $.Deferred();
-      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].influences));
+      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].influences).addClass('active'));
       dfd.done($scope.updateSpread(dfd, 'influences'));
       break;
       case 8:
       var dfd = $.Deferred();
-      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].hope));
+      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].hope).addClass('active'));
       dfd.done($scope.updateSpread(dfd, 'hope'));
       break;
       case 9:
       var dfd = $.Deferred();
-      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].outcome));
+      $('#reading').prepend($('<dd/>').html($scope.model.cards[$scope.model.card].outcome).addClass('active'));
       dfd.done($scope.updateSpread(dfd, 'outcome'));
       break;
     }
